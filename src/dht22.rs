45ae78714e94e73cb137f758cb3535fadf6c4541
maciek.glowka <maciek.glowka@gmail.com>
2022-09-01 22:38:19 +0200

    initial

---
+++ src/dht22.rs
@@ -0,0 +1,91 @@
+use embassy_rp::gpio::Flex;
+use embedded_hal_1::delay::blocking::DelayUs;
+
+const WAIT_STEP: u32 = 5;
+const MAX_WAIT: u32 = 100;
+
+
+#[derive(Debug)]
+pub struct Reading {
+    pub temp: f32,
+    pub hum: f32
+}
+
+pub struct DHT22<P, D>
+{
+    pub pin: P,
+    pub delay: D
+}
+
+impl<'a, P, D> DHT22<Flex<'a, P>, D>
+where P: embassy_rp::gpio::Pin, D: DelayUs
+{
+    pub fn read(&mut self) -> Result<Reading, &str> {
+        let data = self.read_raw()?;
+
+        let raw_temp: u16 = (data[2] as u16) << 8 | data[3] as u16;
+
+        // if the first bit of the 16bit word is set the temp. is negative
+        let temp: f32 = match raw_temp & 0x8000 == 1 {
+            true => -0.1 * (raw_temp & 0x7fff) as f32,
+            false => 0.1 * raw_temp as f32
+        };
+        
+        let raw_hum: u16 = (data[0] as u16) << 8 | data[1] as u16;
+        let hum: f32 = 0.1 * raw_hum as f32;
+
+        Ok(Reading{ temp, hum })
+    }
+
+    fn read_raw(&mut self) -> Result<[u8; 4], &str> {
+        // wake up the sensor by pulling the pin down
+        self.pin.set_as_output();
+        self.pin.set_low();
+        self.delay.delay_us(1000);
+
+        // wait for the pin to go up again and then drop to low for 20-40us
+        self.pin.set_as_input();
+        let _ = wait_for_state(|| self.pin.is_high(), &mut self.delay);
+        let _ = wait_for_state(|| self.pin.is_low(), &mut self.delay);
+
+        // another state flip, 80us for both low and high
+        let _ = wait_for_state(|| self.pin.is_high(), &mut self.delay);
+        let _ = wait_for_state(|| self.pin.is_low(), &mut self.delay);
+
+        // data read starts here
+        let mut buf = [42u8; 4];
+
+        for idx in 0..4 {
+            buf[idx] = self.read_byte();
+        }
+        let checksum = self.read_byte();
+        if checksum != buf.iter().fold(0, |acc, a| acc.wrapping_add(*a)) {
+            return Err("Checksum error");
+        }
+
+        Ok(buf)
+    }
+
+    fn read_byte(&mut self) -> u8 {
+        let mut buf = 0u8;
+        for idx in 0..8 {
+            let _ = wait_for_state(|| self.pin.is_high(), &mut self.delay);
+            let t = wait_for_state(|| self.pin.is_low(), &mut self.delay);
+            
+            if t > 35 {
+                buf |= 1 << 7 - idx;
+            }
+        }
+        buf
+    }
+}
+
+fn wait_for_state<F, D>(f: F, delay: &mut D) -> u32
+where F: Fn()-> bool, D: DelayUs {
+    let mut t = 0;
+    loop {
+        if f() || t > MAX_WAIT { return t; }
+        t += WAIT_STEP;
+        delay.delay_us(WAIT_STEP);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,105 @@
+#![no_std]
+#![no_main]
+#![feature(generic_associated_types)]
+#![feature(type_alias_impl_trait)]
+
+use core::{
+    fmt::Write,
+    panic::PanicInfo
+};
+
+// use defmt::unwrap;
+use embassy_executor::Spawner;
+use embassy_rp::{
+    gpio::Flex,
+    interrupt,
+    usb::Driver
+};
+use embassy_time::{
+    Delay,
+    Duration,
+    Timer,
+};
+use embassy_usb::{
+    Builder,
+    Config
+};
+use embassy_usb_serial::{CdcAcmClass, State};
+
+use futures::future::join;
+use heapless::String;
+
+use defmt_rtt as _;
+
+mod dht22;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+    }
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let irq = interrupt::take!(USBCTRL_IRQ);
+    let driver = Driver::new(p.USB, irq);
+
+    let mut config = Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("M");
+    config.product = Some("piczko W");
+    config.serial_number = Some("1234");
+    config.max_power = 100;
+    config.max_packet_size_0 = 64;
+
+    let mut device_descriptor = [0; 256];
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut device_descriptor,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut control_buf,
+        None
+    );
+
+    let mut usb_class = CdcAcmClass::new(&mut builder, &mut state, 64);
+    let mut usb = builder.build();
+
+    // dht22
+
+    let mut pin_2 = Flex::new(p.PIN_2);
+    let mut dht = dht22::DHT22 { pin: pin_2, delay: Delay };
+
+    join(
+        usb.run(),
+        async {
+            loop {
+                usb_class.wait_connection().await;
+
+                loop {
+                    usb_class.write_packet(b"Hello\n").await;
+
+                    if let Ok(reading) = dht.read() {
+                        let mut s = String::<256>::from("data:");
+                        let _ = write!(s, "{:?}", reading);
+                        usb_class.write_packet(s.as_bytes()).await;
+                        usb_class.write_packet(b"\n").await;
+                    }                    
+                    //
+
+                    Timer::after(Duration::from_secs(2)).await;
+                }
+
+            }
+        }
+    ).await;
+
+}
